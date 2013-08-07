bf9fec7abcad3b34661e5706807599db2db41073
amacleay <a.macleay@gmail.com>
2013-08-07 02:09:23 +0000

    Point at my dotfiles

--- setup.sh
+++ setup.sh
@@ -36,13 +36,9 @@ cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/amacleay/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
 
