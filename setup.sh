218d500e85aa4a4b992cf83571cbd7848afc5693
A.MacLeay <a.macleay@gmail.com>
2015-05-16 13:13:47 -0400

    Fix re.pl RC file installation

--- setup.sh
+++ setup.sh
@@ -26,6 +26,12 @@ ln -sb dotfiles/.vimrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
+if ! [ -d ~/.re.pl ]; then
+  mkdir ~/.re.pl
+fi
+cd ~/.re.pl
+ln -s ~/dotfiles/.re.pl/repl.rc .
+cd ~/
 
 # Setup pathogen for vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
